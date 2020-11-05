67c563fb8a5db999fe6b8148bf97aa12be50cdbc
Andrei Voinea <andreivdev@gmail.com>
2020-11-06 00:05:18 +0200

    Added selectors

--- src/slider.js
+++ src/slider.js
@@ -21,19 +21,52 @@ export default class Slider {
     this.images.forEach((img) => {
       img.setVisible();
     });
+    this.setupSelectors();
     this.slide2Anim();
+  }
+
+  setupSelectors() {
+    let selectorContainer = document.createElement("div");
+    selectorContainer.style.display = "flex";
+    selectorContainer.style.flexDirection = "row";
+    selectorContainer.style.width = "100%";
+    selectorContainer.style.justifyContent = "center";
+
+    for (let i = 0; i < this.images.length; i++) {
+      let selector = document.createElement("div");
+      selector.textContent = "o";
+      selector.style.color = "black";
+      selector.style.padding = "5px";
+      selector.style.fontSize = "24px";
+      selector.dataset.id = i;
+      selector.addEventListener(
+        "click",
+        function (e) {
+          this.currentTranslateMultiplier = e.target.dataset.id;
+          this.translateAnim();
+        }.bind(this)
+      );
+
+      selectorContainer.appendChild(selector);
+    }
+
+    this.element.parentElement.parentElement.appendChild(selectorContainer);
   }
 
   slide2Anim() {
     setTimeout(() => {
-      this.element.style.transition = "transform 0.5s linear";
-      this.element.style.transform =
-        "translate(-" + 100 * this.currentTranslateMultiplier + "%, 0px)";
-      if (this.currentTranslateMultiplier < this.images.length - 1)
-        this.currentTranslateMultiplier++;
-      else this.currentTranslateMultiplier = 1;
+      this.translateAnim();
       this.slide2Anim();
     }, 5000);
+  }
+
+  translateAnim() {
+    this.element.style.transition = "transform 0.5s linear";
+    this.element.style.transform =
+      "translate(-" + 100 * this.currentTranslateMultiplier + "%, 0px)";
+    if (this.currentTranslateMultiplier < this.images.length - 1)
+      this.currentTranslateMultiplier++;
+    else this.currentTranslateMultiplier = 1;
   }
 
   startSlideV1() {
